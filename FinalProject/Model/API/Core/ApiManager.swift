c6c5cf788e41cce77a7589e4d68a43177a0e4c69
Duong Nguyen T <duong.nguyen@asiantech.vn>
2019-09-17 13:44:40 +0700

    remove file realm and api

--- FinalProject/Model/API/Core/ApiManager.swift
+++ FinalProject/Model/API/Core/ApiManager.swift
@@ -12,20 +12,6 @@ import Alamofire
 typealias JSObject = [String: Any]
 typealias JSArray = [JSObject]
 
-enum APIError: Error {
-  case error(String)
-  case errorURL
-
-  var localizedDescription: String {
-    switch self {
-    case .error(let string):
-      return string
-    case .errorURL:
-      return "URL String is error."
-    }
-  }
-}
-
 typealias Completion<Value> = (Result<Value>) -> Void
 typealias APICompletion = (APIResult) -> Void
 
